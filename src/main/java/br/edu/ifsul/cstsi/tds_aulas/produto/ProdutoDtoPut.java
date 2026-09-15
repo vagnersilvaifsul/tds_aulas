918785afc971a6b5f6f78ef271ad655afed2580b
vagnersilvaifsul <vagnersilva@ifsul.edu.br>
2026-09-15 16:09:29 -0300

    feat: construção de controllers, finalizada (retirada das anotações de validação)

--- src/main/java/br/edu/ifsul/cstsi/tds_aulas/produto/ProdutoDtoPut.java
+++ src/main/java/br/edu/ifsul/cstsi/tds_aulas/produto/ProdutoDtoPut.java
@@ -1,34 +1,13 @@
 package br.edu.ifsul.cstsi.tds_aulas.produto;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-
 import java.math.BigDecimal;
 
-/**
- * DTO for {@link Produto}
- */
 public record ProdutoDtoPut(
-        @NotBlank(message = "O nome não pode ser nulo ou vazio")
-        //verifica se está vazio e estabelece como obrigatório (não pode ser nulo)
-        @Size(min = 2, max = 50, message = "Tamanho mínimo de 2 e máximo de 200")
         String nome,
-        @NotBlank(message = "A descrição não pode ser nula ou vazio")
-        //verifica se está vazio e estabelece como obrigatório (não pode ser nulo)
-        @Size(min = 2, max = 255, message = "Tamanho mínimo de 2 e máximo de 255")
         String descricao,
-        @NotNull
-        @Min(0)
         BigDecimal valorDeCompra,
-        @NotNull
-        @Min(0)
         BigDecimal valorDeVenda,
-        @NotNull
-        @Min(0)
         Integer estoque,
-        @NotNull
         Boolean situacao
 ) {
     public ProdutoDtoPut(Produto produto) {
